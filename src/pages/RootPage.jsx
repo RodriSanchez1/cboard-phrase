34f79ad136e0064f6025191d842a8b4407c6a524
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-04-21 11:34:42 -0300

    Chore: update Root Page

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import AppBar from '../components/AppBar/AppBar';
+import CategoriesBar from '../components/CategoriesBar/CategoriesBar';
+import OutputSection from '../components/OutputSection/OutputSection';
+import GridSection from '../components/GridSection/GridSection';
 
 export default function RootPage() {
   return (
-    <div>
-      <AppBar />
+    <>
+      <OutputSection />
+      <CategoriesBar />
+      <GridSection />
       <Outlet />
-    </div>
+    </>
   );
 }
