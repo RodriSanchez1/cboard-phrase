b8563e4b283b217e0282a1f683534a39d09c0d7c
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-04-23 18:55:35 -0300

    Chore: Rename categories folder to category

--- src/pages/RootPage.jsx
+++ src/pages/RootPage.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import AppBar from '../components/AppBar/AppBar';
 import AppHeader from '../features/appHeader/AppHeader';
-import CategoriesBar from '../features/categories/CategoriesBar/CategoriesBar';
+import CategoriesBar from '../features/category/CategoriesBar/CategoriesBar';
 import AppGrid from '../features/grid/AppGrid';
 
 export default function RootPage() {
