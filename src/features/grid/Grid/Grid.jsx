d43b05dc70a60e112d6570f4dc340742e2423675
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-04-24 11:28:39 -0300

    Chore: Functional grid

--- src/features/grid/Grid/Grid.jsx
+++ src/features/grid/Grid/Grid.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import './Grid.css';
-import GridLayout from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 
-//const ReactGridLayout = WidthProvider(RGL);
+import RGL, { WidthProvider } from 'react-grid-layout';
+
+const ReactGridLayout = WidthProvider(RGL);
 
 const arrlayout = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 export default function Grid() {
-  const [layout, setLayout] = useState(generateLayout(6));
+  const [layout, setLayout] = useState(generateLayout(2));
 
   function generateLayout(cols) {
     //const layout = React.Children.map(this.props.children, (child, index) => {
@@ -21,14 +22,16 @@ export default function Grid() {
         i: child.toString(),
       };
     });
-    console.log(layout);
     return layout;
   }
 
   function generateDOM() {
     return arrlayout.map((i) => {
       return (
-        <div key={i.toString()} style={{ border: '1px solid #C00' }}>
+        <div
+          key={i.toString()}
+          style={{ border: '1px solid #C00', backgroundColor: 'white' }}
+        >
           {i}
         </div>
       );
@@ -37,25 +40,23 @@ export default function Grid() {
 
   function onLayoutChange(layout) {
     setLayout(layout);
-    console.log(layout);
   }
 
   return (
     <div className="Grid">
-      <GridLayout
+      <ReactGridLayout
         className="layout"
         items={12}
         rowHeight={80}
-        cols={6}
+        cols={2}
         layout={layout}
-        width={1000}
         onLayoutChange={onLayoutChange}
-        isDraggable={false}
+        // isDraggable={false}
         isResizable={false}
         margin={[10, 10]}
       >
         {generateDOM()}
-      </GridLayout>
+      </ReactGridLayout>
     </div>
   );
 }
