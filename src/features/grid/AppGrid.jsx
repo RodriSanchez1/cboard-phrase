cbb05f0706c265444c87dd0ac7afbaa8f4fad00f
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-04-26 11:48:40 -0300

    Chore: Render tiles

--- src/features/grid/AppGrid.jsx
+++ src/features/grid/AppGrid.jsx
@@ -1,10 +1,25 @@
 import Grid from './Grid/Grid';
 import './AppGrid.css';
+import { useSelector } from 'react-redux';
+import Tile from './Tile/Tile';
+import { selectPhrases } from '../category/categorySlice';
 
 export default function AppGrid() {
+  const phrases = useSelector(selectPhrases);
+
+  function renderTiles() {
+    return phrases.map((phrase) => {
+      return (
+        <div key={phrase.id}>
+          <Tile phrase={phrase} />
+        </div>
+      );
+    });
+  }
+
   return (
     <div className="GridSection">
-      <Grid />
+      <Grid>{renderTiles()}</Grid>
     </div>
   );
 }
--- src/features/grid/Grid/Grid.jsx
+++ src/features/grid/Grid/Grid.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Children } from 'react';
 import PropTypes from 'prop-types';
 import './Grid.css';
 import { Responsive, WidthProvider } from 'react-grid-layout';
@@ -7,12 +7,11 @@ import 'react-resizable/css/styles.css';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
-const arrlayout = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
 Grid.propTypes = {
   cols: PropTypes.object,
   breakpoints: PropTypes.object,
   rowHeight: PropTypes.number,
+  children: PropTypes.node,
 };
 
 Grid.defaultProps = {
@@ -23,63 +22,46 @@ Grid.defaultProps = {
   containerPadding: [0, 0],
 };
 
-export default function Grid({ cols, breakpoints, rowHeight }) {
-  const [layouts, setLayouts] = useState(generateLayouts());
-
+export default function Grid({ cols, breakpoints, rowHeight, children }) {
   function generateLayouts() {
     const layouts = {};
     Object.keys(breakpoints).forEach((bp) => {
       layouts[bp] = generateLayout(cols[bp]);
     });
-    console.log(layouts);
     return layouts;
   }
 
   function generateLayout(cols) {
-    //const layout = React.Children.map(this.props.children, (child, index) => {
-    const layout = arrlayout.map((child, index) => {
+    const layout = Children.map(children, (child, index) => {
       return {
         x: index % cols,
         y: Math.floor(index / cols),
         w: 1,
         h: 1,
-        i: child.toString(),
+        i: child.key,
       };
     });
     return layout;
   }
 
-  function generateDOM() {
-    return arrlayout.map((i) => {
-      return (
-        <div
-          key={i.toString()}
-          style={{ border: '1px solid #C00', backgroundColor: 'white' }}
-        >
-          {i}
-        </div>
-      );
-    });
-  }
-
   function onLayoutChange(currentLayout, layouts) {
-    setLayouts(layouts);
+    // setLayouts(layouts);
   }
 
   return (
     <div className="Grid">
       <ResponsiveReactGridLayout
         className="layout"
-        items={arrlayout.length}
+        items={children.length}
         rowHeight={rowHeight}
         cols={cols}
-        layouts={layouts}
+        layouts={generateLayouts()}
         onLayoutChange={onLayoutChange}
         isDraggable={false}
         isResizable={false}
         margin={[10, 10]}
       >
-        {generateDOM()}
+        {children}
       </ResponsiveReactGridLayout>
     </div>
   );
