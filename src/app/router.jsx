a57c19e6449bdb1a0aed6fd7882998514e7de172
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-01 23:40:39 -0300

    Chore: Create edit page

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import ErrorPage from '../pages/ErrorPage';
 import RootPage from '../pages/RootPage';
 import SettingsPage from '../pages/SettingsPage';
+import EditPage from '../pages/EditPage';
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
       {
         path: 'settings',
         element: <SettingsPage />,
+      },
+      {
+        path: 'edit',
+        element: <EditPage />,
       },
     ],
   },
