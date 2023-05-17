65819c28e14a02b6bba0dd392b1e777e52294ded
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-16 23:42:14 -0300

    Chore: Add full dialog to speech settings

--- src/features/speech/SpeechSettings.jsx
+++ src/features/speech/SpeechSettings.jsx
@@ -1,20 +1,39 @@
-import Stack from '@mui/material/Stack';
 import SpeechLanguageSelect from './controls/SpeechLanguageSelect';
 import VoiceSelect from './controls/VoiceSelect';
 import RateSlider from './controls/RateSlider';
 import VolumeSlider from './controls/VolumeSlider';
 import PitchSlider from './controls/PitchSlider';
 import PlaySampleButton from './controls/PlaySampleButton';
+import FullScreenDialog from '../../components/FullScreenDialog/FullScreenDialog';
+import { Paper } from '@mui/material';
 
 export default function SpeechSettings() {
+  console.log('SpeechSettings');
   return (
-    <Stack maxWidth="280px" p={2} gap={2}>
-      <SpeechLanguageSelect />
-      <VoiceSelect />
-      <RateSlider />
-      <VolumeSlider />
-      <PitchSlider />
-      <PlaySampleButton />
-    </Stack>
+    <FullScreenDialog
+      title="Speech"
+      open={true}
+      // onSave={handleSave}
+      // onClose={handleCancel}
+    >
+      <Paper
+        elevation={3}
+        sx={{
+          mt: 1,
+          height: '80vh',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-evenly',
+        }}
+      >
+        <SpeechLanguageSelect />
+        <VoiceSelect />
+        <RateSlider />
+        <VolumeSlider />
+        <PitchSlider />
+        <PlaySampleButton />
+      </Paper>
+    </FullScreenDialog>
   );
 }
