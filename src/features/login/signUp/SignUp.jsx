4a45b0655dcb68e0e1c05c9bfee33396b9fd6a57
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-20 19:35:42 -0300

    Chore: Add SignUp field valdiation

--- src/features/login/signUp/SignUp.jsx
+++ src/features/login/signUp/SignUp.jsx
@@ -9,15 +9,66 @@ import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import { FormControl, FormHelperText } from '@mui/material';
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required';
+  } else if (values.name.length < 3) {
+    errors.name = 'Invalid name';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 8) {
+    errors.password = 'Password must be at least 8 characters';
+  }
+
+  if (values.password !== values.confirmPassword) {
+    errors.confirmPassword = 'Passwords must match';
+  }
+
+  if (!values.acceptedTerms) {
+    errors.acceptedTerms = 'Required';
+  }
+
+  return errors;
+};
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const [validation, setValidation] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      acceptedTerms: false,
+    },
+    validate,
+    validateOnBlur: validation,
+    validateOnChange: validation,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formik.values);
+    setValidation(true);
+    return formik.handleSubmit();
   };
 
   return (
@@ -32,10 +83,10 @@ export default function SignUp() {
         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
           <LockOutlinedIcon />
         </Avatar>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" sx={{ mb: '16px' }}>
           Sign up
         </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+        <form onSubmit={onSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -46,6 +97,11 @@ export default function SignUp() {
                 id="name"
                 label="Name"
                 size="small"
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                error={formik.errors.name ? true : false}
+                helperText={formik.errors.name}
+                onBlur={formik.handleBlur}
               />
             </Grid>
             <Grid item xs={12}>
@@ -57,6 +113,11 @@ export default function SignUp() {
                 name="email"
                 autoComplete="email"
                 size="small"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                error={formik.errors.email ? true : false}
+                helperText={formik.errors.email}
+                onBlur={formik.handleBlur}
               />
             </Grid>
             <Grid item xs={12}>
@@ -69,6 +130,11 @@ export default function SignUp() {
                 id="password"
                 autoComplete="new-password"
                 size="small"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                error={formik.errors.password ? true : false}
+                helperText={formik.errors.password}
+                onBlur={formik.handleBlur}
               />
             </Grid>
             <Grid item xs={12}>
@@ -81,13 +147,35 @@ export default function SignUp() {
                 id="confirmPassword"
                 autoComplete="confirm-password"
                 size="small"
+                onChange={formik.handleChange}
+                value={formik.values.confirmPassword}
+                error={formik.errors.confirmPassword ? true : false}
+                helperText={formik.errors.confirmPassword}
+                onBlur={formik.handleBlur}
               />
             </Grid>
             <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I accept the Terms of Service and Privacy Policy."
-              />
+              <FormControl
+                error={formik.errors.acceptedTerms ? true : false}
+                required
+              >
+                <FormControlLabel
+                  required
+                  control={
+                    <Checkbox
+                      color="primary"
+                      name={'acceptedTerms'}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                  }
+                  label={
+                    <FormHelperText>
+                      {'I accept the Terms of Service and Privacy Policy.'}
+                    </FormHelperText>
+                  }
+                />
+              </FormControl>
             </Grid>
           </Grid>
           <Button
@@ -98,7 +186,7 @@ export default function SignUp() {
           >
             Sign Up
           </Button>
-        </Box>
+        </form>
       </Box>
     </Container>
   );
