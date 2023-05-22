b2c086b7811e7ce2b97f6843c0331bd290b9cc42
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-22 18:27:31 -0300

    Chore: Add sign up  functionabilities

--- src/features/login/signUp/SignUp.jsx
+++ src/features/login/signUp/SignUp.jsx
@@ -1,5 +1,4 @@
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
@@ -12,11 +11,16 @@ import Container from '@mui/material/Container';
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import { FormControl, FormHelperText } from '@mui/material';
+import { useAddNewUserMutation } from '../loginApi';
 import { validate } from './validateSchema';
+import FetchButton from '../../../components/FetchButton/FetchButton';
 
 export default function SignUp() {
   const [validation, setValidation] = useState(false);
+  const [addNewUser, { isSuccess, isLoading, isError, error: fetchError }] =
+    useAddNewUserMutation();
 
+  console.log(isLoading, isError, fetchError, isSuccess);
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -28,17 +32,20 @@ export default function SignUp() {
     validate,
     validateOnBlur: validation,
     validateOnChange: validation,
-    onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+    onSubmit: (formValues) => {
+      handleSignUp(formValues);
     },
   });
 
-  const onSubmit = (e) => {
+  const preSubmit = (e) => {
     e.preventDefault();
-    console.log(formik.values);
     setValidation(true);
     return formik.handleSubmit();
   };
+
+  async function handleSignUp(formValues) {
+    await addNewUser(formValues);
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -55,7 +62,7 @@ export default function SignUp() {
         <Typography component="h1" variant="h5" sx={{ mb: '16px' }}>
           Sign up
         </Typography>
-        <form onSubmit={onSubmit} noValidate>
+        <form onSubmit={preSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
@@ -147,14 +154,13 @@ export default function SignUp() {
               </FormControl>
             </Grid>
           </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Sign Up
-          </Button>
+          <FetchButton
+            isLoading={isLoading}
+            isError={isError}
+            isSuccess={isSuccess}
+            fetchError={fetchError}
+            name={'Sign Up'}
+          />
         </form>
       </Box>
     </Container>
