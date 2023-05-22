387a73362372e1eb810f2fdd956000f50103d626
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-05-22 18:26:21 -0300

    Chore: Refactor login fields validation

--- src/features/login/login/Login.jsx
+++ src/features/login/login/Login.jsx
@@ -10,26 +10,9 @@ import { Avatar } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useFormik } from 'formik';
 import { useState } from 'react';
+import { validate } from './validateSchema';
 
 import './Login.css';
-
-const validate = (values) => {
-  const errors = {};
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  if (!values.password) {
-    errors.password = 'Required';
-  } else if (values.password.length < 8) {
-    errors.password = 'Password must be at least 8 characters';
-  }
-
-  return errors;
-};
 
 export default function Login() {
   const [validation, setValidation] = useState(false);
