03d125d57add7c130163978d659b1b063826d08f
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-06-09 15:58:00 -0300

    Chore: Change put for get req  on topUsedSentences

--- src/features/reports/reportApi.js
+++ src/features/reports/reportApi.js
@@ -3,14 +3,23 @@ import { cboardPhraseAPI } from '../../services/api';
 const reportApi = cboardPhraseAPI.injectEndpoints({
   endpoints: (builder) => ({
     trackEvent: builder.mutation({
-      query: (body) => ({
-        url: `/report/trackEvent`,
+      query: ({ userId, ...body }) => ({
+        url: `/event/${userId}`,
         method: 'POST',
         body,
       }),
+    }),
+    getTopUsedSentences: builder.query({
+      query: (args) => {
+        const { userId, action, days } = args;
+        return {
+          url: '/reports/topUsedSentences',
+          params: { userId, action, days },
+        };
+      },
     }),
   }),
   overrideExisting: false,
 });
 
-export const { useTrackEventMutation } = reportApi;
+export const { useTrackEventMutation, useGetTopUsedSentencesQuery } = reportApi;
