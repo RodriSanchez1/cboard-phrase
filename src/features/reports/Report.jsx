e0b496ada311a0f1e863cb6cf022d03c1b5d2060
Rodri Sanchez <rodri.sanchez1998@gmail.com>
2023-06-09 15:59:20 -0300

    Chore: Show 5 top used sentences table

--- src/features/reports/Report.jsx
+++ src/features/reports/Report.jsx
@@ -2,31 +2,51 @@ import { Fragment } from 'react';
 import { FormattedMessage } from 'react-intl';
 import FullScreenDialog from '../../components/FullScreenDialog/FullScreenDialog';
 import { useNavigate } from 'react-router';
-import { Grid, Button } from '@mui/material';
+import { Grid } from '@mui/material';
 
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import messages from './Report.messages';
-// import CircularProgress from '@mui/material';
-// import theme from '../../theme';
-// import ModifiedAreaChart from '../../components/ModifiedAreaChart/ModifiedAreaChart';
-// import StatCards from '../../components/StatCards/StatCards';
-// import TableCard from '../../components/TableCard/TableCard';
-// import DoughnutChart from '../../components/DoughnutChart/DoughnutChart';
-// import StatCards2 from '../../components/StatCards2/StatCards2';
-// import Barchart from '../../components/Barchart/Barchart';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
+import TableCard from '../../components/TableCard/TableCard';
+import { useGetTopUsedSentencesQuery } from '../reports/reportApi';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../user/userSlice';
+import './Report.css';
 
 export default function Report() {
   const navigate = useNavigate();
   const handleGoBack = () => {
-    navigate('/');
+    navigate('/settings');
   };
+  const user = useSelector(selectUser);
+
+  const [days, setDays] = useState(10);
+  const [topUsedSentences, setTopUsedSentences] = useState([]);
+
+  const { data, refetch, error, isLoading } = useGetTopUsedSentencesQuery({
+    userId: user.id,
+    days: days,
+    action: 'Start Speech',
+  });
+
+  const handleDaysChange = (event) => {
+    setDays(event.target.value);
+  };
+
+  useEffect(() => {
+    refetch();
+  }, [days, refetch]);
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  useEffect(() => {
+    if (!data) return;
+    setTopUsedSentences(data.topUsedSentences);
+  }, [data]);
 
   return (
     <FullScreenDialog
@@ -37,7 +57,7 @@ export default function Report() {
       enableSave={false}
     >
       <Fragment>
-        {/* <div className="Report__Graph">
+        <div className="Report__Graph">
           <Grid container direction="row" className="Report__Graph__Select">
             <Grid item className="Report__Graph__Select__Item">
               <FormControl variant="outlined">
@@ -45,7 +65,7 @@ export default function Report() {
                   labelId="range-select-label"
                   id="range-select"
                   autoWidth={false}
-                  onChange={this.handleDaysChange}
+                  onChange={handleDaysChange}
                   value={days}
                 >
                   <MenuItem value={10}>
@@ -64,10 +84,11 @@ export default function Report() {
               </FormControl>
             </Grid>
             <Grid item className="Report__Graph__Select__Item">
-              {isFetching && <CircularProgress size={30} thickness={4} />}
+              {/* {isFetching && <CircularProgress size={30} thickness={4} />} */}
             </Grid>
           </Grid>
-          <ModifiedAreaChart
+        </div>
+        {/* <ModifiedAreaChart
             height="200px"
             option={{
               series: [
@@ -85,61 +106,28 @@ export default function Report() {
                 offset: -13,
               },
             }}
-          />
-        </div>
+          /> */}
         <div className="Report__Metrics">
-          <Grid container spacing={3}>
+          <Grid
+            container
+            spacing={3}
+            // alignItems="center"
+            // justify="center"
+            // direction="column"
+          >
             <Grid item lg={8} md={8} sm={12} xs={12}>
-              <StatCards
-                onDetailsClick={this.handleDetailsDialogOpen.bind(this)}
-                data={totals}
-              />
               <TableCard
-                data={topUsed.symbols}
-                tableHead={tablesHead}
-                title={intl.formatMessage(messages.topUsedButtons)}
-              />
-            </Grid>
-            <Grid item lg={4} md={4} sm={12} xs={12}>
-              <DoughnutChart
-                data={symbolSources}
-                title={intl.formatMessage(messages.symbolSources)}
-                height="300px"
-                color={[
-                  theme.palette.primary.dark,
-                  theme.palette.primary.main,
-                  theme.palette.primary.light,
-                ]}
-              />
-              <StatCards2 categoryTotals={categoryTotals} />
-              <Barchart
-                data={topUsed.boards}
-                title={intl.formatMessage(messages.mostUsedBoards)}
+                data={
+                  topUsedSentences.length
+                    ? topUsedSentences
+                    : [{ sentence: 'No data', count: 0 }]
+                }
+                tableHead={['lol']}
+                title={'Top five most used sentences'}
               />
             </Grid>
           </Grid>
-        </div> */}
-        {/* <Dialog
-          onClose={this.handleDialogClose.bind(this)}
-          aria-labelledby="details-dialog"
-          open={this.state.openDetailsDialog}
-          TransitionComponent={Transition}
-          aria-describedby="details-desc"
-        >
-          <DialogContent className={classes.root}>
-            <DialogContentText id="details-dialog-desc"></DialogContentText>
-            <StyledTable
-              data={this.state.detailsData}
-              tableHead={tablesHead}
-              isDense={true}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleDialogClose.bind(this)} color="primary">
-              <FormattedMessage {...messages.close} />
-            </Button>
-          </DialogActions>
-        </Dialog> */}
+        </div>
       </Fragment>
     </FullScreenDialog>
   );
